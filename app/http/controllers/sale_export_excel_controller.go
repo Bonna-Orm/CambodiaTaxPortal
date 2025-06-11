190005e69ac6ea17043faa2de568bc1c141431e0
Orm <ambonna123@gmail.com>
2025-06-11 15:35:35 +0700

    Add function Export Sale By Date

--- app/http/controllers/sale_export_excel_controller.go
+++ app/http/controllers/sale_export_excel_controller.go
@@ -19,14 +19,54 @@ func NewExportController() *ExportController {
 }
 
 func (r *ExportController) ExportExcel(ctx http.Context) http.Response {
+	startDate := ctx.Request().Query("start_date")
+	endDate := ctx.Request().Query("end_date")
 
 	var sales []models.Sale
-	if err := facades.Orm().Query().Find(&sales); err != nil {
+	query := facades.Orm().Query()
+
+	// Optional: Validate date format
+	const layout = "2006-01-02"
+	if startDate != "" {
+		if _, err := time.Parse(layout, startDate); err != nil {
+			return ctx.Response().Status(400).Json(http.Json{
+				"error": "Invalid start date format. Use YYYY-MM-DD.",
+			})
+		}
+	}
+
+	if endDate != "" {
+		if _, err := time.Parse(layout, endDate); err != nil {
+			return ctx.Response().Status(400).Json(http.Json{
+				"error": "Invalid end date format. Use YYYY-MM-DD.",
+			})
+		}
+	}
+
+	// Filter by date if provided
+	if startDate != "" && endDate != "" {
+		query = query.Where("date >= ? AND date <= ?", startDate, endDate)
+	} else if startDate != "" {
+		query = query.Where("date >= ?", startDate)
+	} else if endDate != "" {
+		query = query.Where("date <= ?", endDate)
+	}
+	if err := query.Get(&sales); err != nil {
 		return ctx.Response().Status(500).Json(http.Json{
 			"message": "Failed to fetch sales",
 			"error":   err.Error(),
 		})
+
 	}
+	// Test Return Data Json with Postman
+	//return ctx.Response().Json(http.StatusOK, sales)
+
+	// if err := facades.Orm().Query().Find(&sales); err != nil {
+	// 	return ctx.Response().Status(500).Json(http.Json{
+	// 		"message": "Failed to fetch sales",
+	// 		"error":   err.Error(),
+	// 	})
+	// }
 
 	file := excelize.NewFile()
 	index, err := file.NewSheet("Sheet1")
